27a754085d30a0c8ebe6701b9b02d3322180b2c3
alexcostabd <alex.costa19@fatec.sp.gov.br>
2021-01-03 17:50:30 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,11 +9,17 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
-        public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage ="{0} should be between {2} and {1} caracters")]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
@@ -21,8 +27,13 @@ namespace SalesWebMvc.Models
 
         [Display(Name = "Base Salary R$")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
-        public Department Department { get; set; }
+
+
+        [Required(ErrorMessage = "{0} is required")]
+        public Department Department { get; set; }        
         public int DepartmentId { get; set; }   
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
